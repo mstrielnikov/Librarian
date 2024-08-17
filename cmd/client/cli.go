399169784fda96b5ea11bae088915a0499439c9e
maksym.strielnikov <maksym.strielnikov@capgemini.com>
2024-08-17 22:25:17 +0200

    Refactor env variable processing

---
+++ cmd/client/cli.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+var Env = Environment{
+	configFilePath:              ".env",
+	configExtension:             ".env",
+	allowConfigFileNonExistanse: true,
+	checkEnvVar:                 true,
+	nullable:                    false,
+}
+
+var (
+	// Set defaults from environment variables if available
+	minioEndpoint        = Env.GetEnv("MINIO_ENDPOINT", "").asString()
+	minioAccessKeyID     = Env.GetEnv("MINIO_ACCESS_KEY", "").asString()
+	minioSecretAccessKey = Env.GetEnv("MINIO_SECRET_KEY", "").asString()
+	minioBucketName      = Env.GetEnv("MINIO_BUCKET", "").asString()
+	minioUseSSL          = Env.GetEnv("MINIO_USE_SSL", "").asBool()
+)
+
+// UploadMinioCmd represents the base command when called without any subcommands
+var UploadMinioCmd = &cobra.Command{
+	Use:   "client <bucket> <key> <file>",
+	Short: "Command to upload documents to minio",
+	Long:  `Upload documents to minio by providing file name to a bucket`,
+	Run: func(cli *cobra.Command, args []string) {
+		if len(args) != 6 {
+			log.Fatalf(fmt.Sprintf(`only accepts 5 arguments, got %d`, len(args)))
+		}
+
+		minioClient := NewMinioClient(minioEndpoint, minioAccessKeyID, minioSecretAccessKey, minioUseSSL)
+
+		bucket, err := cli.Flags().GetString("bucket")
+		if err != nil {
+			log.Fatalf(fmt.Sprintf("bucket is not defined: %v", err))
+		}
+
+		filename, err := cli.Flags().GetString("filename")
+		if err != nil {
+			log.Fatalf(fmt.Sprintf("error reading filename %s: %v", filename, err))
+		}
+
+		minioClient.UploadFile(bucket, filename)
+	},
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "client",
+	Short: "User client to upload documents",
+	Long:  `User CLI client to upload documents in minio storage or any S3 compatible one for further processing`,
+	// Run: func(cli *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err = UploadMinioCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.client.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(UploadMinioCmd)
+
+	UploadMinioCmd.Flags().StringVar(&minioEndpoint, "endpoint", minioEndpoint, "MinIO endpoint")
+	UploadMinioCmd.Flags().StringVar(&minioAccessKeyID, "accessKey", minioAccessKeyID, "Access key ID")
+	UploadMinioCmd.Flags().StringVar(&minioSecretAccessKey, "secretKey", minioSecretAccessKey, "Secret access key")
+	UploadMinioCmd.Flags().BoolVar(&minioUseSSL, "ssl", minioUseSSL, "Use SSL")
+	UploadMinioCmd.Flags().StringVar(&minioBucketName, "bucket", minioBucketName, "Bucket name")
+	UploadMinioCmd.Flags().String("filename", "", "Filename to upload")
+}
---
+++ cmd/client/env.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"path"
+	"strconv"
+)
+
+const (
+	yellow = "\033[33m"
+	reset  = "\033[0m"
+)
+
+type EnvVar struct {
+	envName  string
+	envValue string
+}
+
+func (v *EnvVar) asString() string {
+	return v.envValue
+}
+
+func (v *EnvVar) asBool() bool {
+	val, err := strconv.ParseBool(v.envValue)
+	if err != nil {
+		log.Fatalf("Invalid value for : %s. Must be true or false.\n", v.envName)
+	}
+	return val
+}
+
+type Environment struct {
+	configFilePath              string
+	configExtension             string
+	allowConfigFileNonExistanse bool
+	checkEnvVar                 bool
+	nullable                    bool
+}
+
+func NewEnvironment(configFilePath, configExtension string, allowConfigFileNonExistanse, checkEnvVar, nullable bool) *Environment {
+	_, err := os.Stat(configFilePath)
+	if os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configFilePath)
+	}
+
+	return &Environment{
+		configFilePath:              configFilePath,
+		configExtension:             configExtension,
+		allowConfigFileNonExistanse: allowConfigFileNonExistanse,
+		checkEnvVar:                 checkEnvVar,
+		nullable:                    nullable,
+	}
+}
+
+func (e *Environment) envConfigGet(key string) string {
+	_, err := os.Stat(e.configFilePath)
+	if os.IsNotExist(err) {
+		if e.allowConfigFileNonExistanse {
+			warning := fmt.Sprintf("config file %s does not exist or not readable. Env vars will be checked instead", e.configFilePath)
+			log.Println(highlightWarning(warning))
+			return ""
+		} else {
+			log.Fatalf("config file %s not exists", e.configFilePath)
+		}
+	}
+
+	if e.configExtension == "" {
+		e.configExtension = path.Ext(e.configFilePath) //obtain the extension of file
+		if e.configExtension == "" {
+			log.Fatalf("can't determine config file %s extension", e.configFilePath)
+		}
+	}
+
+	viper.SetConfigFile(e.configFilePath)
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("error while reading config file: %v", err)
+	}
+
+	val, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return val
+}
+
+func (e *Environment) GetEnv(key, defaultValue string) *EnvVar {
+	var envVar EnvVar
+	envVar.envName = key
+
+	if val := e.envConfigGet(key); val != "" {
+		envVar.envValue = val
+		return &envVar
+	}
+
+	if e.checkEnvVar {
+		if val := os.Getenv(key); val != "" {
+			envVar.envValue = val
+			return &envVar
+		} else {
+			warning := fmt.Sprintf("env var %s not set", key)
+			log.Println(highlightWarning(warning))
+		}
+	}
+
+	if defaultValue != "" {
+		envVar.envValue = defaultValue
+		return &envVar
+	}
+
+	if e.nullable {
+		warning := fmt.Sprintf("default value for env var %s not set but nullable values allowed. Returning empty string", key)
+		log.Println(highlightWarning(warning))
+	} else {
+		log.Fatalf("env var %s not set. Default value is undefined", key)
+	}
+	envVar.envValue = ""
+	return &envVar
+}
+
+func highlightWarning(warning string) string {
+	return fmt.Sprintf("%s%s%s", yellow, warning, reset)
+}
